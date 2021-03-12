8f029bebaeaff433620de05b37aaf73f7519603d
rachael-amuge <rachaelamuge12@gmail.com>
2021-03-12 12:41:27 +0300

    My Assignment

---
+++ main.js
@@ -0,0 +1,32 @@
+//Variables with stings as their values
+let greetings = 'Hello World';
+let myName = 'Rachael';
+let novel = 'The Great Gatsby';
+let movies = 'Amillion ways to die in the west';
+
+//variables with number as their values
+var x = 4;
+var y = 5;
+var z = 7;
+var p = 9;
+
+//sting concatenation of strings
+const greetings = 'Hello' + ' ' + 'World!';
+
+let myFavourite = chocolate;
+console.log('I love' + myFavourite + 'so much');
+
+//concatenating numbers
+var a = 4;
+var b = 5;
+var c = 2;
+console.log('' + a + b + c );
+
+//condition statement
+var Age = prompt('Enter your age :');
+if(Age>=18){
+    alert('you are free to enjoy');
+}else
+if(Age<18){
+    alert('alcohol is not sold to persons under the age of 18');
+}
